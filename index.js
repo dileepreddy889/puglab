1782c7ba9b22f5b286e5cd302d89a34fe0bc0ce7
NikhithaKolluri8563 <54335512+NikhithaKolluri8563@users.noreply.github.com>
2019-10-26 01:46:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var reqExpress = require('express');
+var loginCookieParser = require('cookie-parser');
+var pug_exp = reqExpress();
+var bodyParser = require('body-parser')
+pug_exp.use(bodyParser.urlencoded({extended: true}));
+pug_exp.use(bodyParser.json());
+
+pug_exp.set('view engine', 'pug');
+pug_exp.use(reqExpress.static('extras'));
+pug_exp.use(loginCookieParser());
+
+pug_exp.get('/', function(req, res){
+  res.render('loginDet')
+});
+
+pug_exp.post('/saveCookies', function(req, res){
+  console.log("Body");
+  console.log(req.body);
+  res.cookie('loginCookie', req.body);
+  res.render('display_Cookie');
+});
+
+pug_exp.post('/displayDetails', function (req,res) {
+  res.render('display_Details',{loginCookie: req.cookies.loginCookie});
+});
+
+pug_exp.listen(3000, function(){
+  console.log("Listening to port 3000")
+});
